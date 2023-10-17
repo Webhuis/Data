c420c549de9abca0de6bec3f75761dd673619cd7
Martin Simons <martin@webhuis.nl>
2023-10-17 21:44:38 +0200

    Data 3-tier with stub ZMQ and luguru logging

--- class_PostgreSQL.py
+++ class_PostgreSQL.py
@@ -4,11 +4,14 @@ import psycopg2 as pq
 
 class PostgreSQL():
   def __init__(self, db="data", user="www_data"):
+    '''
     try:
       self.conn = pg.pool.ThreadedConnectionPool(3, 12, user=user, password='webhu15iio', host='10.68.171.111', port='5432', database=db )
     except (Exception, psycopg2.DatabaseError) as error:
-      error_log.info("Error while connecting to PostgreSQL {}".format(error.args)
-'''
+      PostgreSQL_error_log.info("Error while connecting to PostgreSQL {}".format(error.args)
+    '''
+    pass
+
   def pool_connect(self, cursor):
     self.getconn()
 
@@ -21,7 +24,7 @@ class PostgreSQL():
   def close(self):
     self.cur.close()
     self.conn.close()
-'''
+
 logger.add('/var/log/Data_log/PostgreSQL_event.log', filter = lambda record: 'data' in record['extra'] )
 PostgreSQL_event_log = logger.bind(data = True)
 PostgreSQL_event_log.info('Start Data PostgreSQL event logging')
