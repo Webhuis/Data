0b6aa381b967f8833e948a8908afb68e0e11cc81
Martin Simons <martin@webhuis.nl>
2023-10-20 12:09:27 +0200

    Data processes message, first steps

--- class_Data.py
+++ class_Data.py
@@ -4,6 +4,7 @@
 from loguru import logger
 from class_PostgreSQL import PostgreSQL
 import class_PostgreSQL
+import json
 import os
 import sys
 
@@ -14,6 +15,20 @@ class IterClass(type):
     return iter(cls._allObjects)
 
 class Data(object):
+  '''
+  The Data class architecture has convergence in mind. Convergence is the theoretical model in which agents convergently work towards their desired state.
+  Data tries to provide the requesters with all the available information that is suitable for the requesting agents.
+  The agent itself works convergently towards the desired state, which is defined in the role based policies.
+  This way the information provided by Data enables the agent to make the promises come true.
+
+  Data contains information along the following lines:
+  - Host data
+  - Domain data
+  - Role data
+  The role may require Data to provide the agent with context information and information about other agents.
+
+  Data receives a message from an agent with agent specific information, which triggers a response containing the above information from Data to the agent.
+  '''
   _allObjects = []
 
   __metaclass__ = IterClass
@@ -25,15 +40,36 @@ class Data(object):
 
   def feed(self, message):
     self.message = message
+
+    write_feed(message)
+
+    response = process_message(message)
+
+    return response
+
+  def process_message(message)
+
+    message_json = json.loads(message)
+
+    Data_event_log.info(response_json)
+    response = 'Response' + self.message
+    return response
+
+  def decompose_json(message):
+    self.message = message
     response = 'Response' + self.message
     print(response)
     return response
 
-logger.add('/var/log/Data_log/Data_event.log', filter = lambda record: 'data' in record['extra'] )
+  def write_feed(message):
+    pass
+
+class role(object)
+logger.add('/var/log/Data_log/Data_event.log', rotation="1 day", retention="1 week", compression="bz2", filter = lambda record: 'Data' in record['extra'] )
 Data_event_log = logger.bind(data = True)
 Data_event_log.info('Start Data Data event logging')
 
-logger.add('/var/log/Data_log/Data_error.log', filter = lambda record: 'error' in record['extra'] )
+logger.add('/var/log/Data_log/Data_error.log', rotation="1 day", retention="1 week", compression="bz2", filter = lambda record: 'Data' in record['extra'], level="ERROR")
 Data_error_log = logger.bind(error = True)
-Data_error_log.info('Start Data Data error logging')
+Data_error_log.info('Start Data Data ERROR logging')
 
