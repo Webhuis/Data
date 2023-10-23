1d07989e5d3f466dfd0d0a9b06f71928c88f694e
Martin Simons <martin@webhuis.nl>
2023-10-23 10:55:25 +0200

    Feeding Data

--- class_Data.py
+++ class_Data.py
@@ -4,7 +4,7 @@
 from loguru import logger
 from class_PostgreSQL import PostgreSQL
 import class_PostgreSQL
-import datetime as dt
+from datetime import datetime
 import json
 import os
 import sys
@@ -53,7 +53,7 @@ class Data(object):
     message_json = json.loads(message)
 
     Data_event_log.info(message_json)
-    timestamp = dt.datetime(now)
+    timestamp = datetime(now)
     query = ('insert into feeds.json_in ( message_time, message_in, message_flow ) values ( timestamp, message_json, 0 );')
     response = postgres.pool_query(query) 
     return response
