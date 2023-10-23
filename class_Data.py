4a43789a87ccff1d19559d1d8238afec83aaa344
Martin Simons <martin@webhuis.nl>
2023-10-23 12:09:23 +0200

    Feeding Data

--- class_Data.py
+++ class_Data.py
@@ -57,7 +57,8 @@ class Data(object):
     print(type(timestamp), timestamp)
     query = ("insert into feeds.json_in ( message_time, message_in ) values ( '{}', '{}' );".format( timestamp , message_json ) )
     print(query)
-    response = self.postgres.pool_insert(query) 
+    self.postgres.pool_insert(query) 
+    response = 'Response' + self.message
     return response
 
   def decompose_json(message):
