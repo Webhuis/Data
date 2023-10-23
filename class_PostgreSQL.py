fee274a3f3528c7e6760a5b4853e52983e3158b5
Martin Simons <martin@webhuis.nl>
2023-10-23 16:38:37 +0200

    Feeding Data

--- class_PostgreSQL.py
+++ class_PostgreSQL.py
@@ -18,6 +18,7 @@ class PostgreSQL():
       pg_cursor = pg_conn.cursor()
       pg_cursor.execute(query)
       result = pg_cursor.fetchall()
+      pg_cursor.close()
       pg_conn.close()
     except (Exception, pg.DatabaseError) as error:
       print("Error while selecting from PostgreSQL {}".format(error.args))
@@ -31,6 +32,7 @@ class PostgreSQL():
       pg_cursor.execute(query)
       pg_conn.commit()
       result = pg_cursor.fetchall()[0]
+      pg_cursor.close()
       pg_conn.close()
     except (Exception, pg.DatabaseError) as error:
       print("Error while selecting from PostgreSQL {}".format(error.args))
