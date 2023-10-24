63760cba0d86e4454a072206e7392585af8d9017
Martin Simons <martin@webhuis.nl>
2023-10-24 22:47:35 +0200

    Feed processing to hard_classes

--- class_Feed.py
+++ class_Feed.py
@@ -34,7 +34,7 @@ class Feed(object):
     return query
 
   def insert_feed(self, message):
-    query = "insert into feeds.hard_classes (uqhost, domain, os, ostype, flavor, cpus, arch) values ('{}', '{}', '{}', '{}', '{}', '{}', '{}');".format(self.uqhost, self.domain, self.os, self.ostype, self.flavor, self.cpus, self.arch)
+    query = "insert into feeds.hard_classes (uqhost, domain, os, ostype, flavor, cpus, arch) values ('{}', '{}', '{}', '{}', '{}', '{}', '{}') returning id;".format(self.uqhost, self.domain, self.os, self.ostype, self.flavor, self.cpus, self.arch)
     return query
 
   def check_update(self, values):
