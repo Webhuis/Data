e9eacad03c070b949cbc9b2ff61ba9ca7880d651
Martin Simons <martin@webhuis.nl>
2023-10-24 12:09:56 +0200

    Feeding Data

--- class_PostgreSQL.py
+++ class_PostgreSQL.py
@@ -3,11 +3,13 @@
 from loguru import logger
 import psycopg2 as pg
 from psycopg2 import pool
+from psycopg2.pool import ThreadedConnectionPool as ThCP
+from threading import Semaphore
 
 class PostgreSQL():
   def __init__(self, db="data", user="www_data"):
     try:
-      self.pg_pool = pg.pool.ThreadedConnectionPool(8, 64, user=user, password='we8hu15iio', host='10.68.171.50', port='5432', database=db )
+      self.pg_pool = DataThCP(1, 12, user=user, password='we8hu15iio', host='10.68.171.50', port='5432', database=db )
       PostgreSQL_event_log.info('Start Data PostgreSQL __init__')
     except (Exception, pg.DatabaseError) as error:
       PostgreSQL_error_log.info("Error while connecting to PostgreSQL {}".format(error.args))
@@ -50,6 +52,25 @@ class PostgreSQL():
   def close(self):
     self.cur.close()
     self.conn.close()
+
+class DataThCP(ThCP):
+  def __init__(self, minconn, maxconn, *args, **kwargs):
+    self._semaphore = Semaphore(maxconn)
+    super().__init__(minconn, maxconn, *args, **kwargs)
+
+  def getconn(self, *args, **kwargs):
+    self._semaphore.acquire()
+    try:
+      return super().getconn(*args, **kwargs)
+    except:
+      self._semaphore.release()
+      raise
+
+  def putconn(self, *args, **kwargs):
+    try:
+      super().putconn(*args, **kwargs)
+    finally:
+      self._semaphore.release()
 
 logger.add('/var/log/Data_log/PostgreSQL_event.log', filter = lambda record: 'data' in record['extra'] )
 PostgreSQL_event_log = logger.bind(data = True)
