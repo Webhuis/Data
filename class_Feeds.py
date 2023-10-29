d885d9ae8ae4b52e04bd811f170b73c6943f6c6b
Martin Simons <martin@webhuis.nl>
2023-10-29 19:29:07 +0100

    Feeds as a class

--- class_Feeds.py
+++ class_Feeds.py
@@ -22,7 +22,7 @@ class Feed(object):
   def insert_feed(self):
     timestamp = datetime.now(timezone.utc)
     query = "insert into feeds.json_in ( message_time, message_in ) values ( '{}', '{}' ) returning id;".format( timestamp , self.message_json )
-    self.postgres.pool_insert(query)
+    pg_id_feed = self.postgres.pool_insert(query)
     return pg_id_feed
 
   def update_feed(self):
