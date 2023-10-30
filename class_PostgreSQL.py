431a2c4a9c33b5c284c9cb4704dd83d910cead68
Martin Simons <martin@webhuis.nl>
2023-10-30 01:00:44 +0100

    Feeds as a class

--- class_PostgreSQL.py
+++ class_PostgreSQL.py
@@ -54,6 +54,7 @@ class PostgreSQL():
       PostgreSQL_event_log.info(result)
     except (Exception, pg.DatabaseError) as error:
       result = "Error while inserting into PostgreSQL {}".format(error.args)
+      print(query)
       PostgreSQL_error_log.info(result)
     return result
 
