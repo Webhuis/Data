edc38530599d747dfa6a7669936f190e0ffbc6a6
Martin Simons <martin@webhuis.nl>
2023-10-31 17:22:37 +0100

    Populate Context schema from Feeds, with dedicated loguru

--- class_PostgreSQL.py
+++ class_PostgreSQL.py
@@ -14,14 +14,14 @@ class PostgreSQL():
     for x in object_keys:
       print(x)
       print(x, fd.objects(x))
-    self.PostgreSQL_event = fd.fetch_object(fd.objects, 'PostgreSQL_event')
-    self.PostgreSQL_error = fd.fetch_object(fd.objects, 'PostgreSQL_error')
+    #self.PostgreSQL_event = fd.fetch_object(fd.objects, 'PostgreSQL_event')
+    #self.PostgreSQL_error = fd.fetch_object(fd.objects, 'PostgreSQL_error')
     try:
       self.pg_pool = DataThCP(1, 8, user=user, password='we8hu15iio', host='10.68.171.50', port='5432', database=db )
-      PostgreSQL_event.info('Start Data PostgreSQL __init__')
+      #PostgreSQL_event.info('Start Data PostgreSQL __init__')
     except (Exception, pg.DatabaseError) as error:
-      PostgreSQL_errorg.info("Error while connecting to PostgreSQL {}".format(error.args))
-
+      #PostgreSQL_errorg.info("Error while connecting to PostgreSQL {}".format(error.args))
+      pass
 
   def check_exists(self, query):
     try:
