c40efc095b9afd9da997677335d5910164ed02fe
Martin Simons <martin@webhuis.nl>
2023-10-31 17:19:49 +0100

    Populate Context schema from Feeds, with dedicated loguru

--- class_PostgreSQL.py
+++ class_PostgreSQL.py
@@ -10,7 +10,7 @@ import functions_Data as fd
 
 class PostgreSQL():
   def __init__(self, db="data", user="www_data"):
-    object_keys = fd.objects(keys):
+    object_keys = fd.objects(keys)
     for x in object_keys:
       print(x)
     self.PostgreSQL_event = fd.fetch_object(fd.objects, 'PostgreSQL_event')
