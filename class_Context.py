bb1b1a068881f076b8fa5f0c1d6d2d02b5e0222d
Martin Simons <martin@webhuis.nl>
2023-11-01 21:48:22 +0100

    fqhost_role_view from Context schema with json response and fqhost update

--- class_Context.py
+++ class_Context.py
@@ -37,7 +37,7 @@ class FQHost(object):
 
   def update_fqhost(self):
     self.last_seen = datetime.now(timezone.utc)
-    self.query = ("update context.fqhost set (last_seen) = '{}' returning id").format(self.last_seen)
+    self.query = ("update context.fqhost set (last_seen) = '{}' returning id;").format(self.last_seen)
     self.id_fqhost_update = self.postgres.pool_insert(self.query)
     return id_fqhost_update
 
