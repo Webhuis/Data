71b8cb3d87583530ec27ed319e14d4bc15c6a762
Martin Simons <martin@webhuis.nl>
2023-11-01 16:09:02 +0100

    fqhost_role_view from Context schema with json response

--- class_Context.py
+++ class_Context.py
@@ -19,9 +19,8 @@ class FQHost(object):
       self.query = self.insert_fqhost()
       self.id_fqhost = self.postgres.pool_insert(self.query)
 
-    self.query = ('select uqhost, domain_name, role_code, service_type, service_port from context.fqhost_role where uqhost = {} and domain_name = {};'
+    self.query = ("select uqhost, domain_name, role_code, service_type, service_port from context.fqhost_role where uqhost = '{}' and domain_name = '{}';"
              .format(self.uqhost, self.domain_name))
-    print(self.query)
     self.fqhost_role_view = self.postgres.pool_query(self.query)
     return self.fqhost_role_view
 
