3b34958ad3489795907d37b56f48ce73e42c0a1b
Martin Simons <martin@webhuis.nl>
2023-11-01 14:42:35 +0100

    Populate Context schema from Feeds, with dedicated loguru

--- class_Context.py
+++ class_Context.py
@@ -30,11 +30,10 @@ class FQHost(object):
 
   def insert_fqhost(self):
     self.timestamp = datetime.now(timezone.utc)
-    self.query = ("begin transaction;"
-                  "insert into context.fqhost (uqhost, domain, role_code, timestamp) "
-                  "values ('{}', '{}', '{}', '{}') returning id;".format(
-                   self.uqhost, self.domain, self.role_code, self.timestamp)
-                  "commit;")
+    self.query = ("begin transaction;
+                   insert into context.fqhost (uqhost, domain, role_code, timestamp)
+                   values ('{}', '{}', '{}', '{}') returning id;".format(self.uqhost, self.domain, self.role_code, self.timestamp)
+                   commit;")
     return self.query
 
 class Role(object):
