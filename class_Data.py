5172aeedb428b85bc81b043b4f1c9847699d971f
Martin Simons <martin@webhuis.nl>
2023-11-04 18:58:12 +0100

    Context schema with fqhost and domain data

--- class_Data.py
+++ class_Data.py
@@ -44,7 +44,7 @@ class Data(object):
     self.domain_object = SubDomain(self.domain_name, self.postgres)
     self.domain_data = self.domain_object.get_domain_data()
     #print(self.fqhost_role_view,self.domain_data)
-    json_containers = [ json.dumps(self.fqhost_role_view[0], self.domain_data[0]) ]
+    json_containers = [ self.fqhost_role_view[0], self.domain_data[0] ]
     self.response = json.dumps(json_containers)
     print(self.response)
     return self.response, self.feed_object, self.fqhost_object
