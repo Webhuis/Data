33ac652b15865d179cc09b2d2ec152f1561fee36
Martin Simons <martin@webhuis.nl>
2023-11-10 16:02:03 +0100

    view context fqhost, domain, role containers

--- class_Data.py
+++ class_Data.py
@@ -64,9 +64,9 @@ class Data(object):
     self.organisation_data = self.organisation_object.get_organisation_data(self.organisation_name)
     self.fqhost_data = self.fqhost_object.get_fqhost_services_view()
     self.role_info = self.role_container(role_code)
-    print('fqhost_view', [ self.fqhost_data, self.organisation_data, self.domain_data, self.role_info])
     response_to_json = fd.to_json('fqhost_view', [ self.fqhost_data, self.organisation_data, self.domain_data, self.role_info])
-    self.response_to_json.replace('"', "'")
+    print('response_to_json', response_to_json)
+    response_to_json.replace('"', "'")
     self.response = json.dumps(response_to_json)
 
     return self.response
