35352b7988a15e71bf7321fc189fd7065e51e1ed
Martin Simons <martin@webhuis.nl>
2023-11-10 01:09:04 +0100

    view context fqhost, domain, role containers

--- class_Context.py
+++ class_Context.py
@@ -63,7 +63,7 @@ class Domain(object):
 
     self.query =  ("select organisation_name, domain_data from context.domain where domain_name = '{}';").format(domain_name)
     self.domain_all = self.postgres.pool_query(self.query)
-    print('get_domain_data', self.domain_data)
+    print('get_domain_data', self.domain_all)
     return (self.domain_all[0], self.domain_all[1])
 
 class Role(object):
