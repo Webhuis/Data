ab7e523fc8e6583eb7f86aec963dad0e0789ed2d
Martin Simons <martin@webhuis.nl>
2023-11-12 22:58:23 +0100

    view context fqhost_date, domain, role inetwork containers

--- class_Context.py
+++ class_Context.py
@@ -21,7 +21,7 @@ class FQHost(object):
       self.id_fqhost = self.postgres.pool_insert(self.query)
 
     #self.query = ("select row_to_json(x) from (select uqhost, domain_name, fqhost_data from context.fqhost where uqhost = '{}' and domain_name = '{}') as x;"
-    self.query = ("select fqhost_data from context.fqhost where uqhost = '{}' and domain_name = '{}') as x;"
+    self.query = ("select fqhost_data from context.fqhost where uqhost = '{}' and domain_name = '{}');"
                  .format(self.uqhost, self.domain_name))
     self.fqhost_data = self.postgres.pool_query(self.query)
     print('fqhost_data', self.fqhost_data)
@@ -40,7 +40,7 @@ class FQHost(object):
     organisation_view = fd.to_json(organisation_name, [ self.organisation_data ])
     print('organisation_view', organisation_view)
 
-    self.query = ("select domain_role_data from context.domain_role where domain_name = '{}' and role_code = '{}') as x;"
+    self.query = ("select domain_role_data from context.domain_role where domain_name = '{}' and role_code = '{}');"
                  .format(self.domain_name, self.role_code))
     self.domain_role_data = self.postgres.pool_query(self.query)
     domain_role_view = fd.to_json(domain_role_name, [ self.domain_role_data ])
@@ -52,9 +52,6 @@ class FQHost(object):
                        on ndr.network_name = n.network_name
                      where ndr.domain_name = '{}'
                        and ndr.role_code = '{}' as x;""".format(self.domain_name, self.role_code)
-
-    self.query = ("select row_to_json(x) from (select uqhost, domain_name, fqhost_data from context.fqhost where uqhost = '{}' and domain_name = '{}') as x;"
-                 .format(self.uqhost, self.domain_name))
 
     domain_role_network_view = self.postgres.pool_query(self.query)
     print('domain_role_network_view', self.domain_role_network_view)
