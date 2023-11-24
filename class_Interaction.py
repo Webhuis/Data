1e1e97c255344681b449afbdb3564e268216c367
Martin Simons <martin@webhuis.nl>
2023-11-24 16:21:14 +0100

    Timeout in messaging

--- class_Interaction.py
+++ class_Interaction.py
@@ -5,6 +5,7 @@ from class_Data import Data
 import class_Data
 #import class_ZMQ
 import sys
+from datetime import datetime as dt
 import zmq
 
 import functions_Data as fd
@@ -21,9 +22,14 @@ class Interaction(object):
     self.Interaction_error = fd.fetch_object(Interaction.loggers, 'Interaction_error')
     self.ZMQ_event = fd.fetch_object(Interaction.loggers, 'ZMQ_event')
     self.ZMQ_error = fd.fetch_object(Interaction.loggers, 'ZMQ_error')
+    self.zmq_init()
 
   def run(self):
+    min_sec_num = 0
+    min_sec = dt.now().strftime("%M:%S")
+    min_sec_old = min_sec
     while True:
+
       try:
         b_message = socket.recv()
         print(type(b_message), b_message)
@@ -37,7 +43,7 @@ class Interaction(object):
             s_response = str(response[0])
             #print(type(s_response), s_response)
             #b_response = s_response.encode('utf8')
-            socket.send_string(s_response)
+            iself.socket.send_string(s_response)
             self.ZMQ_event.info(response[0])
           except Exception as e:
             self.ZMQ_error.info('Error sending message {}'.format(e.args))
@@ -46,18 +52,28 @@ class Interaction(object):
           self.Interaction_error.info('Error creating task.{}'.format(e.args))
       except zmq.error.ZMQError as e:
         self.ZMQ_error.info('zmq.error.InterruptedSystemCall {}'.format(e.args))
+        min_sec = dt.now().strftime("%M:%S")
+        min_sec_num += 1
+        if min_sec > 99 and min_sec == min_sec_old
+          self.socket.close()
+          self.zmq_init
+          min_sec_num = 0
+          min_sec = dt.now().strftime("%M:%S")
+          min_sec_old = min_sec
       except Exception as e:
         self.ZMQ_error.info('Error receiving message {}'.format(e.args))
         sys.exit(1)
     else:
-      socket.close()
+      self.socket.close()
+
+def zmq_init():
+
+  self.context = zmq.Context()
+  self.socket = context.socket(zmq.REP)
+  self.socket.setsockopt(zmq.SNDTIMEO, 10000)
+  self.socket.setsockopt(zmq.RCVTIMEO, 10000)
+  self.socket.bind("tcp://10.68.171.111:5309")
 
 for logname in ['Interaction_event', 'Interaction_error', 'ZMQ_event', 'ZMQ_error']:
   fd.add_logger.log = fd.add_logger(logname)
   fd.dict_update(Interaction.loggers, '{}'.format(logname), fd.add_logger.log)
-
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.setsockopt(zmq.SNDTIMEO, 10000)
-socket.setsockopt(zmq.RCVTIMEO, 10000)
-socket.bind("tcp://10.68.171.111:5309")
